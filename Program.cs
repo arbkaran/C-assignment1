4365e511f156f3cd839f7d6d3f9eed5bea7b33ee
arbkaran <karanarb1@gmail.com>
2023-02-14 20:38:06 +0800

    First Commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Antti";
+            int currentScore = 1000;
+            int levelScore = 50;
+            int totalScore = currentScore + levelScore;
+
+            Console.WriteLine("Antti has score before level: " + currentScore);
+            Console.WriteLine("Antti has score after level:: " + totalScore);
+
+
+        }
+    }
+}
