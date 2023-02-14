a5db53f38179fa620a536cbe7b577568ba698334
arbkaran <karanarb1@gmail.com>
2023-02-14 21:09:17 +0800

    Completed assignment 3

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+
+        static void Assignment1()
         {
             string name = "Antti";
             int currentScore = 1000;
@@ -14,7 +15,43 @@ namespace HelloWorld
 
             Console.WriteLine("Antti has score before level: " + currentScore);
             Console.WriteLine("Antti has score after level:: " + totalScore);
+        }
 
+        static void Assignment2()
+        {
+            double x = 10.5, y = 20, multiplier = 0.5;
+            double xMultiplier = x * multiplier;
+            double yMultiplier = y * multiplier;
+            Console.WriteLine("Divided X : " + xMultiplier + " ,y:" + yMultiplier);
+        }
+        /*
+        Include at least the following:
+        - Name
+        - Age
+        - Hometown
+        - Favorite color
+        - Likes rock-music - true/false
+        */
+        static void Assignment3()
+        {
+            string name = "Arup", hometown = "Bangladesh", color = "green";
+            int age = 19;
+            Boolean likeRockMusisc = true;
+            Boolean hasPet = true;
+            Console.WriteLine(
+                 "My Name is " + name + "\n" +
+                 "I am " + age + " years old" + "\n" +
+                 "I am from " + hometown + "\n" +
+                 "My favourite color is " + color + "\n" +
+                 "It is " + likeRockMusisc + " that I like Rock Music" + "\n" +
+                 "It is " + hasPet + " that I have a pet" + "\n"
+            );
+        }
+        static void Main(string[] args)
+        {
+            // Assignment1();
+            // Assignment2();
+            Assignment3();
 
         }
     }
